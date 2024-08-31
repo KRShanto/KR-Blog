b7df19a337682c4e4c1799ee5f8b444e38ca501a
KR Shanto <krshanto2005@gmail.com>
2024-08-31 09:02:32 +0600

    ignore SSG for now. to upload the code in vercel.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,20 @@
-import BlogPage from "./BlogPage";
-import { getData } from "@/lib/getData";
-import { CATEGORY_TAG, POST_TAG } from "@/lib/consts";
+// import BlogPage from "./BlogPage";
+// import { getData } from "@/lib/getData";
+// import { CATEGORY_TAG, POST_TAG } from "@/lib/consts";
+
+// export default async function Page() {
+//   const posts = await getData("api/posts", {
+//     tag: POST_TAG,
+//   });
+//   const categoris = await getData("api/categories", {
+//     tag: CATEGORY_TAG,
+//   });
+
+//   return (
+//     <BlogPage posts={posts} categoris={categoris} selectedCategory={null} />
+//   );
+// }
 
 export default async function Page() {
-  const posts = await getData("api/posts", {
-    tag: POST_TAG,
-  });
-  const categoris = await getData("api/categories", {
-    tag: CATEGORY_TAG,
-  });
-
-  return (
-    <BlogPage posts={posts} categoris={categoris} selectedCategory={null} />
-  );
+  return <div></div>;
 }
